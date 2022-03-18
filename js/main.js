2c3b750fb0627c1106e80176900b2f92ef9f90c5
Xacnio <alperenitec@yandex.com>
2022-03-18 15:43:24 +0300

    sticky header with scroll & content height fix for footer

--- js/main.js
+++ js/main.js
@@ -9,4 +9,23 @@ $(document).ready(() => {
             $('.header-bs').removeClass('mobile-enable').addClass('mobile-disable');
         }
     })
+
+    var sticky = $('.header').height();
+    var lastY = null;
+    window.onscroll = function () {
+        if (window.pageYOffset > sticky) {
+            const currentY = window.pageYOffset;
+            if (lastY != null) {
+                if (currentY >= lastY) {
+                    $('.header').addClass('sticky').removeClass("sticky-hide");
+                } else if (!$('.header-bs').hasClass('mobile-enable')) {
+                    $('.header').removeClass('sticky').addClass("sticky-hide");
+                }
+            }
+            lastY = window.pageYOffset;
+
+        } else if (!$('.header-bs').hasClass('mobile-enable')) {
+            $('.header')./*removeClass('sticky').*/removeClass('sticky-hide');
+        }
+    };
 })
