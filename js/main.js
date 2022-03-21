fb259927a371773e737720fd05df2dc194238851
Xacnio <alperenitec@yandex.com>
2022-03-22 02:25:32 +0300

    new pages - fixes

--- js/main.js
+++ js/main.js
@@ -28,4 +28,39 @@ $(document).ready(() => {
             $('.header')./*removeClass('sticky').*/removeClass('sticky-hide');
         }
     };
+    $(function () {
+        $('img.lazy').Lazy({
+            effect: 'fadeIn',
+            effectTime: 250,
+        });
+    });
+    $('.header-user-dropdown').prev().click((e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        $('.header-user-dropdown').addClass("open");
+    })
+    $(document).click(function () {
+        var headerUserDropdown = $('.header-user-dropdown')
+        headerUserDropdown.removeClass("open");
+    });
+
+    $('.profile-menu a').click((e) => {
+        var item = $(e.target)
+        if (item.parent('li').hasClass("active")) {
+            return;
+        }
+        var href = $(e.target).attr("tab-href");
+        var tab = $(`.tab[tab-name="${href}"]`);
+        $('.profile-menu li').removeClass("active");
+        item.parent('li').addClass("active")
+        if (tab.length != 0) {
+            $('.tab').removeClass("in")
+            setTimeout(() => {
+                $('.tab').removeClass("open")
+                setTimeout($('.tab').removeClass("open"), 1000)
+                tab.addClass("open")
+                setTimeout(() => tab.addClass("in"), 150)
+            }, 150)
+        }
+    })
 })
